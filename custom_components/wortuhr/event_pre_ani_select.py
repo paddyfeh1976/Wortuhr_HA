12959cb1d8717b96fa8501f1af1a00e1e97557f2
TGqnguZqrU7vI5 <patrick.fehrmann@gmail.com>
2026-06-26 10:19:29 +0200

    Vorbereitungen für Animationen

---
+++ custom_components/wortuhr/event_pre_ani_select.py
@@ -0,0 +1,60 @@
+"""Select entity for Wortuhr event pre-animation."""
+
+from __future__ import annotations
+
+from homeassistant.components.select import SelectEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN, EVENT_ANIMATION_OPTIONS
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    host = config_entry.data.get(CONF_HOST)
+
+    device_info = DeviceInfo(
+        identifiers={(DOMAIN, host)},
+        name="Wortuhr",
+        manufacturer="Wortuhr",
+        model="HTTP API",
+        configuration_url=f"http://{host}",
+    )
+
+    async_add_entities([WortuhrPreAnimationSelect(hass, config_entry, device_info, host)])
+
+
+class WortuhrPreAnimationSelect(SelectEntity):
+    _attr_has_entity_name = True
+    _attr_name = "Voranimation"
+    _attr_options = list(EVENT_ANIMATION_OPTIONS.keys())
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        device_info: DeviceInfo,
+        host: str,
+    ) -> None:
+        self.hass = hass
+        self.config_entry = config_entry
+        self._attr_device_info = device_info
+        self._host = host
+        self._attr_unique_id = f"wortuhr_pre_animation_select_{config_entry.entry_id}"
+        self._current = " "
+
+    @property
+    def current_option(self) -> str | None:
+        return self._current
+
+    async def async_select_option(self, option: str) -> None:
+        if option not in EVENT_ANIMATION_OPTIONS:
+            return
+        self._current = option
+        self.async_write_ha_state()
