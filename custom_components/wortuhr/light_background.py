d3e5a23702b705c8899c5dec8c6c60e0c563fc18
TGqnguZqrU7vI5 <patrick.fehrmann@gmail.com>
2026-07-17 18:00:40 +0200

    Test Backgroundlicht ohne Dimmung  alles wieder reverted

--- custom_components/wortuhr/light_background.py
+++ custom_components/wortuhr/light_background.py
@@ -7,12 +7,12 @@ import logging
 from homeassistant.components.light import (
     LightEntity,
     ColorMode,
-    ATTR_XY_COLOR,
+    ATTR_RGB_COLOR
 )
-from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, STATE_ON
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util.color import color_rgb_to_hex, color_xy_to_RGB
@@ -42,12 +42,9 @@ async def async_setup_entry(
 
 class WortuhrBackgroundLight(LightEntity, RestoreEntity):
     _attr_has_entity_name = True
-    _attr_name = "Hintergrund Farbe"
-    _attr_icon = "mdi:palette"
-    
-
-    _attr_supported_color_modes = {ColorMode.XY}
-    _attr_color_mode = ColorMode.XY
+    _attr_name = "Minuten Punkte"
+    _attr_icon = "mdi:led-strip"
+    _attr_supported_features = 0
 
     def __init__(
         self,
@@ -62,7 +59,7 @@ class WortuhrBackgroundLight(LightEntity, RestoreEntity):
         self._entry_id = config_entry.entry_id
         self._attr_unique_id = f"wortuhr_background_light_{self._entry_id}"
         self._is_on = True
-        self._xy_color = (0.13, 0.03)  # Entspricht dem alten Standard #1A00BC
+        self._rgb_color = (26, 0, 188)  # Entspricht dem alten Standard #1A00BC
         
         # Ziel-Verknüpfung zum Background-Option Select
         self._target_select_unique_id = f"wortuhr_background_option_{self._entry_id}"
@@ -70,33 +67,46 @@ class WortuhrBackgroundLight(LightEntity, RestoreEntity):
     async def async_added_to_hass(self) -> None:
         """Wird aufgerufen, wenn die Entität zu Home Assistant hinzugefügt wurde."""
         await super().async_added_to_hass()
-        
-        last_state = await self.async_get_last_state()
+
+        try:
+            last_state = await self.async_get_last_state()
+        except Exception as err:
+            _LOGGER.debug("Konnte letzten Zustand für Minutenlicht nicht lesen: %s", err)
+            last_state = None
+
         if last_state is not None:
             self._is_on = last_state.state == STATE_ON
 
-            xy = last_state.attributes.get(ATTR_XY_COLOR)
-            if xy:
-                self._xy_color = tuple(xy)
+            rgb_value = last_state.attributes.get(ATTR_RGB_COLOR)
+            if rgb_value:
+                self._rgb_color = tuple(rgb_value)
 
     @property
     def is_on(self) -> bool:
         return self._is_on
 
     @property
-    def xy_color(self) -> tuple[float, float] | None:
-        return self._xy_color
+    def supported_color_modes(self) -> set[ColorMode]:
+        return {ColorMode.RGB}
+
+    @property
+    def color_mode(self) -> ColorMode:
+        return ColorMode.RGB if self._is_on else ColorMode.OFF
+
+    @property
+    def rgb_color(self) -> tuple[int, int, int] | None:
+        """Gibt die aktuell gesetzte RGB-Farbe an Home Assistant zurück."""
+        return self._rgb_color
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         self._is_on = True
         
         # 1. Farbwahl verarbeiten und in Hex umrechnen
-        if ATTR_XY_COLOR in kwargs:
-            self._xy_color = kwargs[ATTR_XY_COLOR]
+        if ATTR_RGB_COLOR in kwargs:
+            self._rgb_color = kwargs[ATTR_RGB_COLOR]
 
         # Erzeuge den Hex-String (z.B. "#1A00BC") für die API
-        r, g, b = color_xy_to_RGB(*self._xy_color)
-        hex_color = color_rgb_to_hex(r, g, b).upper()
+        hex_color = color_rgb_to_hex(*self._rgb_color).upper()
         await async_set_setting(self.hass, self._host, "Bgc", "#" + hex_color)
 
         # 2. Synchronisation: Wenn das Select auf "Aus" steht, schalte es auf "Immer"
